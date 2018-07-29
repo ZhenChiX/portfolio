451088417a43ea70386276f58271cdcdbb883991
StarGale <zhenchix@hotmail.com>
2018-07-29 04:52:38 -0700

    update layout

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -9,10 +9,23 @@ class Main extends Component {
         </header>
         <div className='Main-logo'></div>
         <div className='Main-intro'>
-        <h1>Hello,I'm Chi!</h1>
-        <h1>Web Developer</h1>
+          <h1>Hello,I'm Chi.</h1>
+          <h1>I'm a front-end web developer.</h1>
         </div>
+        <div className="Main-skill">
+          <h1>SKILLS : </h1>
+          <ul>
+            <li className="Floating-1">JavaScript</li>
+            <li className="Floating-2">HTML5</li>
+            <li className="Floating-3">CSS3</li>
+            <li className="Floating-4">JQuery</li>
+            <li className="Floating-4">AJAX</li>
+            <li className="Floating-3">React</li>
+            <li className="Floating-2">Express</li>
+            <li className="Floating-1">MongoDB</li>
 
+          </ul>
+        </div>
       </div>
     );
   }
