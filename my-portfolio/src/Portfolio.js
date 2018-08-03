0b44c382a0e0eba6b923f7f96d1c992d6296c76d
StarGale <zhenchix@hotmail.com>
2018-08-02 19:28:50 -0700

    upload mockup

--- my-portfolio/src/Portfolio.js
+++ my-portfolio/src/Portfolio.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Modal extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <img className="Mobile" alt="project-preview" />
+                <img className="Mobile" alt="project-preview" />
+
+
+
+            </div>
+        );
+    }
+}
+
+
 class Portfolio extends Component {
     render() {
         return (
@@ -10,20 +29,20 @@ class Portfolio extends Component {
                 {/* <h1>PORTFOLIO</h1> */}
                 <div>
                     <h1>Game Tracker</h1>
-                    <img className="Desktop" alt="project-preview" />>
-                    <img className="Mobile" alt="project-preview" />
+                    <img className="Desktop" src={require("./mockup/desktop-gt.png")} alt="project-preview" />
+
                     <p></p>
                 </div>
                 <div>
                     <h1>Movie Royale</h1>
-                    <img className="Desktop" alt="project-preview" />
-                    <img className="Mobile" alt="project-preview" />
+                    <img className="Desktop" src={require("./mockup/desktop-mr2.png")} alt="project-preview" />
+
                     <p></p>
                 </div>
                 <div>
                     <h1>Code Potato</h1>
-                    <img className="Desktop" alt="project-preview" />
-                    <img className="Mobile" alt="project-preview" />
+                    <img className="Desktop" src={require("./mockup/desktop-cp.png")} alt="project-preview" />
+
                     <p></p>
                 </div>
 
@@ -35,4 +54,4 @@ class Portfolio extends Component {
 
 
 export default Portfolio;
-
+// export default Modal;
